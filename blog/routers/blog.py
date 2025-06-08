1b406badaedecd12578df83c2943d2e9fef4b2b5
Murtza ali <alimurtzaa2004@gmail.com>
2025-06-08 23:13:33 +0530

    bug

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -18,7 +18,7 @@ def all_blogs(db: Annotated[Session, Depends(get_db)], current_user: Annotated[s
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id: int, db: Annotated[Session, Depends(get_db)], current_user: Annotated[schemas.User, Depends(get_current_user)]):
-    return blog.sohw(id, db)
+    return blog.show(id, db)
 
 @router.post('/',status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Annotated[Session, Depends(get_db)], current_user: Annotated[schemas.User, Depends(get_current_user)]):
